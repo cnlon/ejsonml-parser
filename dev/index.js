2d9ced3c0bc59c392be73896f1a9d220dfb376c6
longhao <longhaohe@gmail.com>
2016-06-13 22:10:54 +0800

    Move jsParse here, rewrite ejmlParser and jsParser

--- dev/index.js
+++ dev/index.js
@@ -1,18 +1,27 @@
-import parse from '../lib'
+const Parser = require('../lib')
 
 const tpl =
 `
-<div class="className" style="background-color: red;"  :style="style">
+<div *if="true" class="className" style="background-color: red;"  :style="style">
   <strong :text="name+','"></strong>你好
   <!--comment!!-->
 </div>
 `
-const jml = parse(tpl)
+const parser = new Parser(tpl)
+const ejml = parser.ejml
+const hjson = parser.parse()
+const hjs = parser.parse(true)
 
 window.onload = function () {
   const tplNode = document.getElementById('tpl')
   tplNode.innerText = tpl
 
-  const resNode = document.getElementById('res')
-  resNode.innerText = JSON.stringify(jml, null, '  ')
+  const ejmlNode = document.getElementById('jml')
+  ejmlNode.innerText = JSON.stringify(ejml, null, 2)
+
+  const hjsonNode = document.getElementById('hjson')
+  hjsonNode.innerText = JSON.stringify(hjson, null, 2)
+
+  const hjsNode = document.getElementById('hjs')
+  hjsNode.innerText = JSON.stringify(hjs, null, 2)
 }
